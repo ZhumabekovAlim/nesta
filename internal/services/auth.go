4bb0e1bb82cec7ced1b11668d81a796eb5d1486b
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2026-02-17 12:38:30 +0500

    otp fix

--- internal/services/auth.go
+++ internal/services/auth.go
@@ -204,6 +204,7 @@ func (s *AuthService) VerifyOTP(ctx context.Context, phone, code string) (TokenP
 	}
 
 	latest, err := s.OTP.LatestByPhone(ctx, normalizedPhone)
+	fmt.Println()
 	if err != nil {
 		return TokenPair{}, ErrOTPNotFound
 	}
